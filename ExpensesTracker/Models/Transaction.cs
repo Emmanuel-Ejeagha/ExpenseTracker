a61bee2163756c5ceecfc581effb14284e2751a1
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-01-08 12:57:20 +0100

    Add FormattedAmount

--- ExpensesTracker/Models/Transaction.cs
+++ ExpensesTracker/Models/Transaction.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ExpensesTracker.Models;
@@ -7,10 +8,31 @@ public class Transaction
 {
     public int TransactionId { get; set; }
     [Column(TypeName = "decimal(18,2)")]
+    [Range(1,int.MaxValue,ErrorMessage = "Amount should be greater than 0")]
     public decimal Amount { get; set; }
     public string? Note { get; set; }
     public DateTime Date { get; set; } = DateTime.Now;
-
+    [Range(1,int.MaxValue,ErrorMessage = "Please select a category.")]
     public int CategoryId { get; set; }
-    public Category Category { get; set; }
+    public Category? Category { get; set; }
+
+
+    [NotMapped]
+    public string? CategoryTitleWithIcon
+    {
+        get
+        {
+            return Category == null ? "" : Category.Icon + " " + Category.Title;
+        }
+    }
+
+    [NotMapped]
+    public string? FormattedAmount
+    {
+        get
+        {
+            return ((Category == null || Category.Type == "Expense") ? "- " : "+ ") + Amount.ToString("'₦'###,###,##0");
+
+        }
+    }
 }
