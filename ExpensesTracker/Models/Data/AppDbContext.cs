bf64d8538ee4f27d982b94507b4362551c1a00b3
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-01-08 12:56:06 +0100

    Remove OnModel

--- ExpensesTracker/Models/Data/AppDbContext.cs
+++ ExpensesTracker/Models/Data/AppDbContext.cs
@@ -13,13 +13,13 @@ public class AppDbContext : DbContext
     public DbSet<Category> Categories { get; set; }
     public DbSet<Transaction> Transactions { get; set; }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Category>()
-            .HasMany(c => c.Transactions)
-            .WithOne(t => t.Category)
-            .HasForeignKey(t => t.CategoryId)
-            .OnDelete(DeleteBehavior.Restrict);
-    }
+    // protected override void OnModelCreating(ModelBuilder modelBuilder)
+    // {
+    //     modelBuilder.Entity<Category>()
+    //         .HasMany(c => c.Transaction)
+    //         .WithOne(t => t.Category)
+    //         .HasForeignKey(t => t.CategoryId)
+    //         .OnDelete(DeleteBehavior.Restrict);
+    // }
 
 }
